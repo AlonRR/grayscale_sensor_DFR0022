d2c769f1969ae207d9c217bf9976343a933fc13b
Alon A. Rabinowitz <alonrab@outlook.com>
2025-01-27 12:24:02 +0200

    added grayscale sensor reading to main loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include <Arduino.h>
 
+#define GRAYSCALE_PIN 25
+
 void setup()
 {
     Serial.begin(115200);
+    pinMode(GRAYSCALE_PIN, INPUT);
 }
 
 void loop()
 {
+    int val;
+    val = analogRead(GRAYSCALE_PIN); // connect grayscale sensor to Analog 0
+    Serial.println(val, DEC);        // print the value to serial
+    delay(100);
 }
